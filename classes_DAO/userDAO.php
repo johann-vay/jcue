bb9027b2222628b6feba69d607dc86ae887e9f64
Johann <Johann@PC-Johann>
2016-05-24 20:59:04 +0200

    Méthode userDetails --> classe USerDAO

--- classes_DAO/userDAO.php
+++ classes_DAO/userDAO.php
@@ -19,12 +19,25 @@ class UserDAO {
     
     
     public function usersList(){
-        $query = 'SELECT  id, adresse, codePostal, ville, mail, telephone, login, password, type from utilisateur';
+        $query = 'SELECT  id, adresse, codePostal, ville, mail, telephone, login, password, type '
+                . 'FROM utilisateur';
         $arrayUsers = Connection::query($query);
         foreach ($arrayUsers as $user) {
             $users[] = new User($user[0], $user[1], $user[2], $user[3], $user[4], $user[5], $user[6], $user[7], $user[8]);
         }
         return $users;
+    }
+    
+    public function userDetails($userId){
+        $query = 'SELECT id, adresse, codePostal, ville, mail, telephone, login, password, type '
+                . 'FROM utilisateur '
+                . 'WHERE id = '.$userId;
+        $arrayDetails = Connection::query($query);
+        foreach ($arrayDetails as $user) {
+            $user[] = new User($user[0], $user[1], $user[2], $user[3], $user[4], $user[5], $user[6], $user[7], $user[8]);
+        }
+        
+        return $user;
     }
 
 }
