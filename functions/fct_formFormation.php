bce954386abc16a04f66b58e98efa95d32d0d5ca
Johann <Johann@PC-Johann>
2016-05-29 20:55:02 +0200

    Modifications formulaires (debut)

--- functions/fct_formFormation.php
+++ functions/fct_formFormation.php
@@ -1,18 +1,28 @@
 <?php
 
+function formulaireAjoutFormation(){
+    $formation = new Formation(NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+    return formulaireFormation($formation);
+}
+
+function formulaireModificationFormation($idFormation){
+    $formationDAO = new formationDAO();
+    $formation = $formationDAO->formationDetails($idFormation);
+    return formulaireFormation($formation);
+}
+
 function formulaireFormation($formation) {
 
     $html = '<form action=".?page=validationFormulaireFormation" method="POST" class="form-horizontal">
-            <input type="hidden" name="id" value="">'
-            . champTexte('Intitulé', 'intitule')
-            . champDate('Année début','anneeDebut')
-            . champDate('Année fin','anneeFin')
-            . champTexte('Nom établissement','nomEtablissement')
-            . champTexte('Ville établissement','villeEtablissement')
-            . champTexte('Diplôme visé','diplomeVise')
-            . champTexte('Diplôme obtenu','diplomeObtenu')
-            . champSelectQuery('id_cv', 'id_cv', 'SELECT `id` FROM cv',$valeur=null)
-            . btnSubmit()
+            <input type="hidden" name="id" value="'.$formation->getId().'">'
+            . champTexte('Intitulé', 'intitule', $formation->getIntitule())
+            . champDate('Année début','anneeDebut', $formation->getAnneeDebut())
+            . champDate('Année fin','anneeFin', $formation->getAnneeFin())
+            . champTexte('Nom établissement','nomEtablissement', $formation->getNomEtablissement())
+            . champTexte('Ville établissement','villeEtablissement', $formation->getVilleEtablissement())
+            . champTexte('Diplôme visé','diplomeVise', $formation->getDiplomeVise())
+            . champTexte('Diplôme obtenu','diplomeObtenu', $formation->getDiplomeObtenu())
+            . btnSubmit('validerFormation')
             . '</form>';
     return $html;
 }
