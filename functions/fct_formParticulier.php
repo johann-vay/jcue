ecc192e5ce27fe3e6d150f2488802c367f676cf3
Quentin <Quentin@PC-Quentin.home>
2016-05-29 22:11:26 +0200

    Fct_formOffre.php

--- functions/fct_formParticulier.php
+++ functions/fct_formParticulier.php
@@ -1,14 +1,26 @@
 <?php
 
+function formulaireAjoutParticulier(){
+    $particulier = new Particulier(NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+    return formulaireParticulier($particulier);
+}
+
+function formulaireModificationParticulier($idParticulier){
+    $particulierDAO = new particulierDAO();
+    $particulier = $particulierDAO->particulierDetails($idParticulier);
+    return formulaireParticulier($particulier);
+}
+    
 function formulaireParticulier($particulier) {
 
     $html = '<form action=".?page=validationFormulaireParticulier" method="POST" class="form-horizontal">
-            <input type="hidden" name="id" value="">'
-            . champTexte('Nom', 'nom')
-            . champTexte('Prénom','prenom')
-            . champTexte('URL CV vidéo','urlVideo')
-            . champSelectQuery('id_cv', 'id_cv', 'SELECT `id` FROM cv',$valeur=null)
-            . btnSubmit()
+            <input type="hidden" name="id" value="'.$particulier->getId().'">'
+            . champTexte('Libellé', 'libelle', $particulier->getLibelle())
+            . champTexte('Durée','duree', $particulier->getDuree())
+            . champTexte('Decription mission','descriptionMission', $particulier->getDecriptionMission())
+            . champDate('Date début','dateDebut', $particulier->getDateDebut())
+            . champSelectQuery('Type Contrat', 'id_typeContrat', 'SELECT `id` FROM typeContrat',$valeur=null)
+            . btnSubmit('validerParticulier')
             . '</form>';
     return $html;
 }
